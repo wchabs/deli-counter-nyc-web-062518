521568dbf5c07f6b5b79602fe53811e8d5b39435
William Chabla <w_chabla@hotmail.com>
2018-06-21 21:41:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,15 @@
 
 def line(katz_deli)
   if katz_deli.size > 1
-    print "The line is currently: "
-    katz_deli.each do |person|
-      print
+    print "The line is currently:"
+    katz_deli.each_with_index { |person, index|
+    print " #{index + 1}. #{person}"
+    }
+  else
+    puts "The line is curently empty."
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts ""
 end
