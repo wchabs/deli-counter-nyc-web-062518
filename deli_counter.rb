59ca98b1c497eb3577adeaf73dc06b7089c3a33a
William Chabla <w_chabla@hotmail.com>
2018-06-21 21:49:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size > 1
-    puts "Currently serving #{katz_deli.shift}."
-  else
-    puts "There is nobody waiting to be served!"
-  end
+  #if katz_deli.size > 1
+  #  puts "Currently serving #{katz_deli.shift}."
+  #else
+  #  puts "There is nobody waiting to be served!"
+  #end
 end
