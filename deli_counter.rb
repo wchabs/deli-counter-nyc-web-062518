fbe06f2fbf3caeb4d20bac21fab0da72b3c04fe6
William Chabla <w_chabla@hotmail.com>
2018-06-21 22:17:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@
 def line(katz_deli)
   if katz_deli.size > 1
     puts "The line is currently:"
-    katz_deli.each do |person|
-      print " #{katz_deli.index(person) + 1}. #{person}"
-    end
-    #katz_deli.each_with_index { |person, index|
-    #print " #{index + 1}. #{person}"
-    #}
+    #katz_deli.each do |person|
+    #  print " #{katz_deli.index(person) + 1}. #{person}"
+    #end
+    katz_deli.each_with_index { |person, index|
+    print " #{index + 1}. #{person}"
+    }
   else
     puts "The line is currently empty."
   end
