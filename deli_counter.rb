6412791e67a4939840aa38da415a0db4a75a9dbc
William Chabla <w_chabla@hotmail.com>
2018-06-21 21:45:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,10 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts ""
+  puts "You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size > 1
+    puts "Currently serving #{katz_deli.shift}."
 end
